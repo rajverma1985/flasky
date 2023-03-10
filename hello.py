24891e1cd01e5bfde6bed2f9635692562156cc23
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-03-10 20:53:47 +0100

    adding requirement to the commit

--- hello.py
+++ hello.py
@@ -3,8 +3,11 @@ from flask import Flask
 app =Flask(__name__)
 
 
-@app.route('/<name>')
-def hello(name):
-    return f'Hi {name}'
+@app.route('/')
+def hello():
+    return f'Welcome to Flask'
+
+
+
 if __name__=="__main__":
     app.run(debug=True, port=5000)
