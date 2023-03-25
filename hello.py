263de5710c135cc5cfa684ef65ccdabb5156f01e
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-03-25 20:10:04 +0100

    adding templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, abort
+from flask import Flask, make_response, abort, render_template
 
 app = Flask(__name__)
 
@@ -7,15 +7,14 @@ app = Flask(__name__)
 def hello():
     response = make_response('<h1>This is a cookie inside a document</h1>')
     response.set_cookie('testcookie', '40')
-    return response
+    return render_template('index.html')
 
 
-@app.route('/username/<id>')
-def user(id):
-    user = load_user(id)
+@app.route('/username/<name>')
+def user(name):
     if not user:
         abort(404)
-    return '<h1>Hello {}, your id is {}</h1>'.format(user.name, user.id)
+    return render_template('index.html', name=name)
 
 
 if __name__ == "__main__":
