811e071fc3a8be0a0b27b4f2a6da16881051bf0e
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-04-15 22:46:31 +0200

    exploring the power of moment

--- hello.py
+++ hello.py
@@ -2,16 +2,28 @@ from flask import Flask, make_response, abort, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config["SECRET_KEY"] = "some_random_key_here"
 
+
+# Creating a form object from Flaskform class
+
+class NewForm(FlaskForm):
+    name = StringField('What is your Name', validators=[DataRequired()])
+    submit = SubmitField('Submit Form')
+
+
 @app.route('/')
 def hello():
     response = make_response('<h1>This is a cookie inside a document</h1>')
     response.set_cookie('testcookie', '40')
-    return render_template('index.html', current_time = datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/username/<name>')
