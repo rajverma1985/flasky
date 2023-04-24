658bc8cc06b8f21f1fa8fb33c1c207d221bbeaea
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-04-24 21:02:32 +0200

    added password verification to model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # model definition
@@ -15,10 +16,23 @@ class Role(db.Model):
 
 class User(db.Model):
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     # this is the foreign key colum for users table which refers to roles tables id column
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(pwhash=self.password_hash, password=password)
 
     def __repr__(self):
         return "<User %r>" % self.username
