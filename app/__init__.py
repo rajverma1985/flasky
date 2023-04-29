5270c4eadb06d3044442dcd68039359b6989dc1d
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-04-29 10:29:39 +0200

    adding email feature to send emails

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,13 @@ from flask_moment import Moment
 from config import config
 from flask_login import LoginManager
 
-
 bootstrap = Bootstrap()
 moment = Moment()
 mail = Mail()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
